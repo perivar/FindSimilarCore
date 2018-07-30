34d509b33d2522f5d2bb9360d027eb09de3d9996
Per Ivar Nerseth <perivar@nerseth.com>
2018-07-30 14:53:12 +0200

    added release scan all test bat file

---
+++ FindSimilar/Run-Release-Scan-All.bat
@@ -0,0 +1,6 @@
+@echo off
+
+cd "C:\Users\pnerseth\My Projects\FindSimilarCore\FindSimilar\bin\Release\netcoreapp2.1\win-x64\publish"
+FindSimilar scan "C:\Users\pnerseth\Amazon Drive\Documents\Audio\FL Projects" -l "C:\Users\pnerseth\My Projects\findsimilar.log" -d "C:\Users\pnerseth\My Projects\fingerprint.db"
+
+pause
