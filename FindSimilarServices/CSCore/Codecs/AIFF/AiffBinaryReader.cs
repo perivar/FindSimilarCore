2c287c0a221441cde05c28f7d4247dd188efa62b
Per Ivar Nerseth <perivar@nerseth.com>
2018-09-12 21:33:40 +0200

    using Naudio methods to remove warnings

--- FindSimilarServices/CSCore/Codecs/AIFF/AiffBinaryReader.cs
+++ FindSimilarServices/CSCore/Codecs/AIFF/AiffBinaryReader.cs
@@ -31,7 +31,7 @@ namespace CSCore.Codecs.AIFF
         public uint ReadUInt32()
         {
             var buffer = ReadBytes(4);
-            return (uint) ((buffer[0] << 24) |
+            return (uint)((buffer[0] << 24) |
                            (buffer[1] << 16) |
                            (buffer[2] << 8) |
                            buffer[3]);
@@ -40,14 +40,14 @@ namespace CSCore.Codecs.AIFF
         public short ReadInt16()
         {
             var buffer = ReadBytes(2);
-            return (short) ((buffer[0] << 8) |
+            return (short)((buffer[0] << 8) |
                             (buffer[1]));
         }
 
         public ushort ReadUInt16()
         {
             var buffer = ReadBytes(2);
-            return (ushort) ((buffer[0] << 8) |
+            return (ushort)((buffer[0] << 8) |
                              (buffer[1]));
         }
 
@@ -56,7 +56,7 @@ namespace CSCore.Codecs.AIFF
             if (_binaryReader.BaseStream.CanSeek)
                 _binaryReader.BaseStream.Seek(count, SeekOrigin.Current);
             else
-                _binaryReader.ReadBytes((int) count);
+                _binaryReader.ReadBytes((int)count);
         }
 
         private byte[] ReadBytes(int count)
@@ -70,30 +70,33 @@ namespace CSCore.Codecs.AIFF
             return bytes;
         }
 
-        //copied from aiff.cc -> http://worldfoundry.org/doxygen/html/aiff_8cc_source.html#l00633
+        //copied from https://github.com/naudio/NAudio/blob/master/NAudio/Utils/IEEE.cs
+        #region ConvertFromIeeeExtended
+        /// <summary>
+        /// Converts an IEEE 80-bit extended precision number to a
+        /// C# double precision number.
+        /// </summary>
+        /// <param name="bytes">The 80-bit IEEE extended number (as an array of 10 bytes).</param>
+        /// <returns>A C# double precision number that is a close representation of the IEEE extended number.</returns>
         private double ConvertFromIeeeExtended(byte[] bytes)
         {
+            if (bytes.Length != 10) throw new Exception("Incorrect length for IEEE extended.");
             double f;
             int expon;
-            ulong hiMant, loMant;
+            uint hiMant, loMant;
 
-            expon = ((bytes[0] & 0x7F) << 8) | (bytes[1] & 0xFF);
-            hiMant = ((ulong) (bytes[2] & 0xFF) << 24)
-                     | ((ulong) (bytes[3] & 0xFF) << 16)
-                     | ((ulong) (bytes[4] & 0xFF) << 8)
-                     | (ulong) (bytes[5] & 0xFF);
-            loMant = ((ulong) (bytes[6] & 0xFF) << 24)
-                     | ((ulong) (bytes[7] & 0xFF) << 16)
-                     | ((ulong) (bytes[8] & 0xFF) << 8)
-                     | (ulong) (bytes[9] & 0xFF);
+            expon = ((bytes[0] & 0x7F) << 8) | bytes[1];
+            hiMant = (uint)((bytes[2] << 24) | (bytes[3] << 16) | (bytes[4] << 8) | bytes[5]);
+            loMant = (uint)((bytes[6] << 24) | (bytes[7] << 16) | (bytes[8] << 8) | bytes[9]);
 
             if (expon == 0 && hiMant == 0 && loMant == 0)
+            {
                 f = 0;
+            }
             else
             {
-                if (expon == 0x7FFF)
+                if (expon == 0x7FFF)    /* Infinity or NaN */
                 {
-                    /* Infinity or NaN */
                     f = double.NaN;
                 }
                 else
@@ -104,20 +107,19 @@ namespace CSCore.Codecs.AIFF
                 }
             }
 
-            if ((bytes[0] & 0x80) == 0x80)
-                return -f;
-            return f;
+            if ((bytes[0] & 0x80) == 0x80) return -f;
+            else return f;
         }
+        #endregion
 
         private double ldexp(double x, int exp)
         {
             return x * Math.Pow(2, exp);
         }
 
-        //# define UnsignedToFloat(u)         (((double)((long)(u - 2147483647L - 1))) + 2147483648.0)
         private double UnsignedToFloat(ulong u)
         {
-            return ((long) (u - 2147483647L - 1) + 2147483648.0);
+            return (((double)((long)(u - 2147483647L - 1))) + 2147483648.0);
         }
     }
 }
