48abf91208a90bc2ebe8e176cee021da5e88b125
Per Ivar Nerseth <perivar@nerseth.com>
2018-12-08 19:50:07 +0100

    updated scan all

--- FindSimilar/DAW-Run-Release-Scan-All.bat
+++ FindSimilar/DAW-Run-Release-Scan-All.bat
@@ -1,6 +1,11 @@
 @echo off
 
 cd /D "D:\Development Projects\Own Projects\FindSimilarCore\FindSimilar\bin\Release\netcoreapp2.1\win-x64\publish"
-FindSimilar scan "H:\SAMPLES\ACID LOOPS & SAMPLES" -l "D:\Development Projects\Own Projects\findsimilar.log" -e "D:\Development Projects\Own Projects\findsimilar_error.log" -d "D:\Development Projects\Own Projects\fingerprint.db"
+FindSimilar scan "H:\SAMPLES\ACID LOOPS & SAMPLES" ^
+-l "D:\Development Projects\Own Projects\findsimilar.log" ^
+-e "D:\Development Projects\Own Projects\findsimilar_error.log" ^
+-d "D:\Development Projects\Own Projects\fingerprint.db"
 
 pause
+
+@echo off
